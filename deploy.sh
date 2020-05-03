a4437c270a55a35f65681e1f3367825f21aebf96
German Timoshin <razorkam@gmail.com>
2020-05-03 16:45:40 +0300

    Synchronize old files with server

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ rm -f  /etc/systemd/system/klumba_courier_bot.service
 cp -f ./klumba_courier_bot.service /etc/systemd/system/
 systemctl start klumba_courier_bot
 systemctl enable klumba_courier_bot
+systemctl daemon-reload
